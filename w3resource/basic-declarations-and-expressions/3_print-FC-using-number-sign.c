a897746060aad15bebcc7a4a656d7b30586a8e4d
unknown <saifur.rahman.tamim1@gmail.com>
2023-03-11 19:12:48 +0600

    initial commit

---
+++ w3resource/basic-declarations-and-expressions/3_print-FC-using-number-sign.c
@@ -0,0 +1,12 @@
+/*
+3. Write a C program to print a block F using hash (#), where the F has a height of six characters and width of five and four characters. And also to print a big 'C'.
+*/
+#include<stdio.h>
+
+main()
+{
+    printf("Name: MD. Saifur Rahman Tamim \nPersonal Portfolio: https://sr-tamim.vercel.app\n\n");
+
+    printf("######\n#\n#\n#####\n#\n#\n#"); // capital F
+    printf("\n\n   ######\n ##      ##\n#\n#\n#\n#\n#\n ##      ##\n   ######\n"); // capital C
+}
