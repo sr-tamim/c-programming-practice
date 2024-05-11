f396060439233149fd22e131e67c30b48047d310
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-05-11 11:55:33 +0600

    refactor: Implement linear search algorithm in search-linear.c

---
+++ search-linear.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int linearSearch(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = linearSearch(arr, n, x);
+    (result == -1) ? printf("Element is not present in array\n") : printf("Element is present at index %d\n", result);
+    return 0;
+}
